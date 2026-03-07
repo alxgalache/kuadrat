15b9ec1b2b32bb85013eb80a571d494f97c9650b
axgalache <axgalache@gmail.com>
2026-03-07 11:40:47 +0100

    Disable Stripe developer assistant tools in client library initialization.

--- client/lib/stripe.js
+++ client/lib/stripe.js
@@ -6,7 +6,13 @@ export function getStripePromise() {
   if (!stripePromise) {
     const key = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
     if (key) {
-      stripePromise = loadStripe(key)
+      stripePromise = loadStripe(key, {
+        developerTools: {
+          assistant: {
+            enabled: false
+          }
+        }
+      })
     }
   }
   return stripePromise
